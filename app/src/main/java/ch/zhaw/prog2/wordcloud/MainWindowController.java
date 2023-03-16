b0bebfee54cec121b26e71c41e593055acb0120b
esposdie <esposdie@students.zhaw.ch>
2023-03-16 16:01:25 +0100

    Workshop FXML with but

---
+++ app/src/main/java/ch/zhaw/prog2/wordcloud/MainWindowController.java
@@ -0,0 +1,36 @@
+package ch.zhaw.prog2.wordcloud;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+public class MainWindowController {
+
+    @FXML
+    private Label labelTitel;
+
+    @FXML
+    private VBox lableTitel;
+
+    @FXML
+    private TextField textEingabe;
+
+    @FXML
+    private TextArea textHistory;
+
+    @FXML
+    void hinzufuegenText(ActionEvent event) {
+
+    }
+
+    @FXML
+    void leerenTextEingabe(ActionEvent event) {
+
+    }
+
+}
+
